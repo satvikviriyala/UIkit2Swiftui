a12dc757a1631669d78bb48ee9916ac7d9d01106
Satvik <satvikviriyala@gmail.com>
2024-05-21 15:39:45 +0530

    Initial Commit

---
+++ UIkit2Swiftui/UIkit2SwiftuiApp.swift
@@ -0,0 +1,17 @@
+//
+//  UIkit2SwiftuiApp.swift
+//  UIkit2Swiftui
+//
+//  Created by Satvik Viriyala on 21/05/24.
+//
+
+import SwiftUI
+
+@main
+struct UIkit2SwiftuiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
